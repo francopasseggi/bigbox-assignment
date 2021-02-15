9022a363490f8ac2f12f1ea9fed6b3a874193810
Franco Passeggi <francopasseggi@gmail.com>
2021-02-15 17:48:37 -0300

    added home page

--- bigbox/urls.py
+++ bigbox/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import BoxList, BoxDetail, ActivityList, ActivityDetail, search_form, home
 
 urlpatterns = [
-    path('', home, name='home')
+    path('', home, name='home'),
     path('search/', search_form, name='box-search'),
     path('box/', BoxList.as_view(), name='box-list'),
     path('box/<int:pk>/', BoxDetail.as_view(), name='box-detail'),
